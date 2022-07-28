3a58c29ccc64577ecd089e576dbaf01485abb33a
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2022-07-28 12:54:21 -0600

    Revert "added possible path to calendar"

--- event_calendar/manage.py
+++ event_calendar/manage.py
@@ -1,7 +1,10 @@
+
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
 
 
 def main():
@@ -15,6 +18,8 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
 
 
 if __name__ == "__main__":
